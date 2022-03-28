dd13ae6d9b4574433a9bd9f1b5d18aff374c39ba
AlexanderGrn <s.green@list.ru>
2022-03-28 23:49:15 +0300

    Modal window add

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let name1 = 'Hannah';
+let name2 = 'David';
+console.log('Hello %s, hello %s!', name1, name2);
+let num1 = 5;
+let num2 = 7;
+console.log('Num 1 is %s, num 2 is %s, and num 1 add to num 2 is %s.', num1, num2, num1+num2)
